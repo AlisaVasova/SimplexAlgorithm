b074ca17586a7755e46f305619140ea1a4fc6869
Alice <vlasovaru931@gmail.com>
2022-10-24 19:04:02 +0300

    может так сработает

--- Simplex Algorithm/Tests/test_test1.py
+++ Simplex Algorithm/Tests/test_test1.py
@@ -1,19 +1,5 @@
+import math
 
-import pytest
-
-def setup_module(module):
-    #init_something()
-    pass
-
-def teardown_module(module):
-    #teardown_something()
-    pass
-
-def test_upper():
-    assert 'foo'.upper() == 'FOO'
-    
-def test_isupper():
-    assert 'FOO'.isupper()
-    
-def test_failed_upper():
-    assert 'foo'.upper() == 'FOo'
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
