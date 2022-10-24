6239dfa4617b014e4086a1b817c13b1e02a908b2
Alice <vlasovaru931@gmail.com>
2022-10-24 23:47:45 +0300

    Add Sonar Token

--- Simplex Algorithm/Tests/test_test1.py
+++ Simplex Algorithm/Tests/test_test1.py
@@ -9,3 +9,6 @@ import decision
 
 def test_one():
    assert decision.is_there_solution(2, [[1, 1],[0,0]], 1) == True
+
+def test_two():
+   assert decision.is_there_solution(2, [[-1, -1],[-2,-2]], 1) == False
