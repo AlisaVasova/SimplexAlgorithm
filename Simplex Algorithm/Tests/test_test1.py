cad4fe4ab37b87c8cdecd59b03745f40731b3a01
Alice <vlasovaru931@gmail.com>
2022-10-24 22:44:57 +0300

    ф

--- Simplex Algorithm/Tests/test_test1.py
+++ Simplex Algorithm/Tests/test_test1.py
@@ -4,6 +4,9 @@ def test_sqrt():
    num = 25
    assert math.sqrt(num) == 5
 
+import sys
+sys.path.append('..')
+import decision
 
 def test_one():
    assert decision.is_there_solution(2, [[1, 1],[0,0]], 1) == True
