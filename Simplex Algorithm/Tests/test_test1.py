bd65bddb6679a7a687f5b671a12f6e2474ad9736
Alice <vlasovaru931@gmail.com>
2022-10-28 11:14:18 +0300

    Add some tests

--- Simplex Algorithm/Tests/test_test1.py
+++ Simplex Algorithm/Tests/test_test1.py
@@ -25,7 +25,7 @@ def test_is_not_ved_st():
 def test_is_decision():
     n = 4
     m = 2
-    cel_func = [-1, -2, 0, 3]
+    cel_func = [1, 2, 0, 3]
     array = [[1, 0, 2, 1],[4, 1, 6, 0]]
     bdr = [4, 14]
     basis = [2, 4]
@@ -35,7 +35,7 @@ def test_is_decision():
 def test_is_not_decision():
     n = 4
     m = 2
-    cel_func = [1, 2, 0, 3]
+    cel_func = [-1, -2, 0, 3]
     array = [[1, 0, -2, 1],[4, 1, 6, 0]]
     bdr = [-4, -14]
     basis = [2, 4]
