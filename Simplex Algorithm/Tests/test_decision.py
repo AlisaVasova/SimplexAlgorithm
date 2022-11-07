572e1aa2bbfbc3c9cc7d603e9098cac275ceb391
Alice <vlasovaru931@gmail.com>
2022-11-07 22:17:22 +0300

    delete +1

--- Simplex Algorithm/Tests/test_decision.py
+++ Simplex Algorithm/Tests/test_decision.py
@@ -2,10 +2,10 @@ import decision
 from unittest.mock import patch
 
 def test_is_sol():
-   assert decision.is_there_solution(2, [[1, 1],[0,0]], 1) == True
+   assert decision.is_there_solution([[1, 1],[0,0]], 0) == True
 
 def test_is_not_sol():
-   assert decision.is_there_solution(2, [[-1, -1],[-2,-2]], 1) == False
+   assert decision.is_there_solution([[-1, -1],[-2,-2]], 0) == False
 
 def test_print_table():
     n = 4
@@ -18,23 +18,20 @@ def test_print_table():
     assert decision.print_table(basis, bdr, array, n, m, cel_func, delts) == True
 
 def test_is_ved_st():
-   assert decision.ved_stolbec([2,3], 1) == 2
+   assert decision.ved_stolbec([2,3], 2) == 2
 
 def test_is_not_ved_st():
-   assert decision.ved_stolbec([-1,-2], 1) == -1
+   assert decision.ved_stolbec([-1,-2], 2) == -1
 
 def test_preobr():
     n = 4
     m = 2
-    cel_func = [1, 2, 0, 3]
     array = [[1, 0, 2, 1],[4, 1, 6, 0]]
     bdr = [4, 14]
-    basis = [2, 4]
-    delts = [0,0,0,0]
     ved_str = 0
     ved_stolb = 1
     
-    array, bdr = decision.preobr(ved_str, ved_stolb, array, m, n, bdr, cel_func, delts, basis)
+    array, bdr = decision.preobr(ved_str, ved_stolb, array, m, n, bdr)
 
 def test_is_decision():
     n = 4
