feb3ef65a0954dcfb31dbd373b252149a2a2c817
Alice <vlasovaru931@gmail.com>
2022-11-11 00:09:02 +0300

    try cod s interneta

--- Simplex Algorithm/Tests/test_main.py
+++ Simplex Algorithm/Tests/test_main.py
@@ -1,8 +1,33 @@
 import main
 from unittest.mock import patch
+from unittest.mock import call
+
+class MockInputFunction:
+    def __init__(self, return_value=None, side_effect=None):
+        self.return_value = return_value
+        self.side_effect = side_effect
+        self.mock_calls = []
+        self._orig_input_fn = __builtins__['input']
+
+    def _mock_input_fn(self, prompt=None):
+        return_value = self.return_value\
+            if self.side_effect is None\
+            else self.side_effect[len(self.mock_calls)]
+        self.mock_calls.append(call(prompt))
+        if prompt:
+            print(prompt + str(return_value))
+        else:
+            print(str(return_value))
+        return return_value
+
+    def __enter__(self):
+        __builtins__['input'] = self._mock_input_fn
+
+    def __exit__(self, type, value, traceback):
+        __builtins__['input'] = self._orig_input_fn
 
 def test_input():
-    with patch('__builtin__.input', side_effect = [4, 2, 1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) as input:
+    with MockInputFunction(side_effect=[4, 2, 1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]):
         n, m, cel_func, ogr = main.input_model()
         assert n == 4
         assert m == 2
