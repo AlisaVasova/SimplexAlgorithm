88c0f0530b9671063b5150ef20bf3317eb3ab739
Alice <vlasovaru931@gmail.com>
2022-11-10 23:49:48 +0300

    а так?

--- Simplex Algorithm/Tests/test_main.py
+++ Simplex Algorithm/Tests/test_main.py
@@ -2,7 +2,7 @@ import main
 from unittest.mock import patch
 
 def test_input():
-    with patch('builtins.input') as input:
+    with patch('__builtin__.input') as input:
         input.side_effect = [4, 2, 1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         n, m, cel_func, ogr = main.input_model()
         assert n == 4
